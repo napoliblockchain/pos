3a60554ec961b971f7d2b92ecd141db9a0fa645d
Sergio Casizzone <jambtc@gmail.com>
2020-09-23 16:58:14 +0200

    fix yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,8 @@
 <?php
+$yiiVersion = 'yii-1.1.22';
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+$yii=dirname(__FILE__).'/../'.$yiiVersion.'/framework/yii.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
