a0482246fa1e93d41f0a370129ef293bb8a599b9
Sergio Casizzone <jambtc@gmail.com>
2020-09-25 13:35:52 +0200

    receive price true

--- protected/commands/ReceiveCommand.php
+++ protected/commands/ReceiveCommand.php
@@ -106,7 +106,7 @@ class ReceiveCommand extends CConsoleCommand
 				$criteria->compare('to_address',$invoice->to_address);
 				$criteria->addCondition("invoice_timestamp > " .$invoice->invoice_timestamp);
 				$criteria->addCondition("blocknumber > " .$invoice->blocknumber);
-				$criteria->compare('token_price',$invoice->token_price,false); // false -> valore identico
+				$criteria->compare('token_price',$invoice->token_price,true); // false -> valore identico
 
 				$transactions = Tokens::model()->findAll($criteria);
 				//echo '<pre>'.print_r($transactions,true).'</pre>';
